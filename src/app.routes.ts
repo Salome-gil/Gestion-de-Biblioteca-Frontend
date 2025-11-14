a14345910cf8e9f39f66d8aec952a82d00f28628
DESKTOP-7ST3MPC\Janus <mafe.palacio53@gmail.com>
2025-11-13 23:47:31 -0500

    se modifican los archivos app.config.ts y app.routes.ts

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,23 +1,49 @@
 import { Routes } from '@angular/router';
-import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
-import { Notfound } from './app/pages/notfound/notfound';
+import { Dashboard } from 'src/app/features/dashboard/dashboard';
+import { AppLayout } from 'src/app/shared/components/layout/layout.component';
+
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { authRoutes } from 'src/app/features/auth/auth.routes';
+
+import { BibliotecaListComponent } from 'src/app/features/biblioteca/biblioteca-list/biblioteca-list.component';
+import { CategoriaListComponent } from 'src/app/features/categoria/categoria-list/categoria-list.component';
+import { ClienteListComponent } from 'src/app/features/cliente/cliente-list/cliente-list.component';
+import { MaterialBibliograficoListComponent } from 'src/app/features/material-bibliografico/material-bibliografico-list/material-bibliografico-list.component';
+import { PerfilComponent } from 'src/app/features/perfil/perfil.component';
+import { PrestamoListComponent } from 'src/app/features/prestamo/prestamo-list/prestamo-list.component';
+import { ReservaListComponent } from 'src/app/features/reserva/reserva-list/reserva-list.component';
+import { SancionListComponent } from 'src/app/features/sancion/sancion-list/sancion-list.component';
+import { SedeListComponent } from 'src/app/features/sede/sede-list/sede-list.component';
+import { UsuarioListComponent } from 'src/app/features/usuario/usuario-list/usuario-list.component';
+
 
 export const appRoutes: Routes = [
-    {
-        path: '',
-        component: AppLayout,
-        children: [
-            { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
-        ]
-    },
-    { path: 'landing', component: Landing },
-    { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
-    { path: '**', redirectTo: '/notfound' }
+  {
+    path: 'auth',
+    children: authRoutes,
+    //canActivate: [LoginGuard] 
+  },
+
+  {
+    path: '',
+    component: AppLayout,
+    canActivate: [AuthGuard], 
+    children: [
+      { path: '', component: Dashboard },
+      { path: 'dashboard', component: Dashboard },
+
+      { path: 'bibliotecas', component: BibliotecaListComponent },
+      { path: 'categorias', component: CategoriaListComponent },
+      { path: 'clientes', component: ClienteListComponent },
+      { path: 'materiales', component: MaterialBibliograficoListComponent },
+      { path: 'prestamos', component: PrestamoListComponent },
+      { path: 'reservas', component: ReservaListComponent },
+      { path: 'sanciones', component: SancionListComponent },
+      { path: 'sedes', component: SedeListComponent },
+      { path: 'usuarios', component: UsuarioListComponent },
+      { path: 'perfil', component: PerfilComponent},
+    ]
+  },
+
+  { path: '**', redirectTo: 'auth/login' }
 ];
