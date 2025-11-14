d526c7d541f4bee6eb9a1f8040014c24b1e59fd1
Salome-gil <salomechanci@gmail.com>
2025-11-14 11:30:26 -0500

    fix: cambio en apiservice

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/enviroment';
-import { PaginationParams } from '../models/api-response.model';
+import { PaginationParams } from '../models/api-response.models';
 
 @Injectable({
   providedIn: 'root'
